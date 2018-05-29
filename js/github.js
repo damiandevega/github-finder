88363c27372d0d63abfb30baa44506433de19b07
Damian de Vega <damiandevega@gmail.com>
2018-05-28 19:43:37 -0700

    first commit

---
+++ js/github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+        this.client_id = 'e6b875fa42dc5c0d44b9';
+        this.client_secret = '8b0088d20c09baec58410027d06d47b98fe813c0';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
